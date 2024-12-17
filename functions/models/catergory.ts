2445357a258037efe549dc965394e4f890f563fd
unknown <bhagavanmarpadaga@gmail.com>
2024-12-17 09:47:07 +0530

    setup default route

--- functions/models/catergory.ts
+++ functions/models/catergory.ts
@@ -2,20 +2,18 @@ import mongoose from "mongoose";
 import { ICategory } from "../types";
 
 const categorySchema = new mongoose.Schema<ICategory>({
-    name:{
-        type:String
-    },
-    parent:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Catergoy",
-    },
-    isRoot:{
-        type:Boolean,
-        optional:true
-    }
-})
+  name: {
+    type: String,
+  },
+  parent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Catergoy",
+  },
+  isRoot: {
+    type: Boolean,
+    optional: true,
+  },
+});
 
-
-
-const Catergoy = mongoose.model("Category",categorySchema)
-export default Catergoy
+const Catergoy = mongoose.model("Category", categorySchema);
+export default Catergoy;
--- functions/router/index.ts
+++ functions/router/index.ts
@@ -3,5 +3,9 @@ import v1router from './api'
 const apiRouter = express.Router()
 
 apiRouter.use("/api",v1router)
+apiRouter.get("/",(req,res)=>{
+    res.json({message:"Please see the githib read me and hit it accordingly",link:"https://github.com/BhagavanMarpadaga/CategoryManagement/blob/master/Readme.md"})
+})
+
 
 export default apiRouter
